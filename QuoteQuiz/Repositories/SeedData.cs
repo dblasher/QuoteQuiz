149ef229841c730a76dc7ee871137c79da22f0c6
dblasher <dblasher90@gmail.com>
2018-11-29 15:00:33 -0800

    SeedData bug was due to a setting in Program.cs

--- QuoteQuiz/Repositories/SeedData.cs
+++ QuoteQuiz/Repositories/SeedData.cs
@@ -1,6 +1,7 @@
 ﻿using QuoteQuiz.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
